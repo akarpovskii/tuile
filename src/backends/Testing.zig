048c258fc06657e809e5a22b33826f32a2be795d
akarpovskii <an.karpovskii@gmail.com>
2024-04-24 21:44:47 +0400

    Implement testing backend

---
+++ src/backends/Testing.zig
@@ -0,0 +1,83 @@
+const std = @import("std");
+const internal = @import("../internal.zig");
+const Backend = @import("Backend.zig");
+const Vec2 = @import("../Vec2.zig");
+const events = @import("../events.zig");
+const display = @import("../display/display.zig");
+const render = @import("../render/render.zig");
+
+const Testing = @This();
+
+frame_buffer: std.ArrayListUnmanaged(render.Cell),
+
+window_size: Vec2,
+
+frame: render.Frame,
+
+pub fn create(window_size: Vec2) !*Testing {
+    const self = try internal.allocator.create(Testing);
+    var buffer = std.ArrayListUnmanaged(render.Cell){};
+    try buffer.resize(internal.allocator, window_size.x * window_size.y);
+    const frame = render.Frame{
+        .size = window_size,
+        .buffer = buffer.items,
+        .area = .{
+            .min = Vec2.zero(),
+            .max = window_size,
+        },
+    };
+    frame.clear(display.color("black"), display.color("white"));
+
+    self.* = .{
+        .window_size = window_size,
+        .frame_buffer = buffer,
+        .frame = frame,
+    };
+    return self;
+}
+
+pub fn destroy(self: *Testing) void {
+    self.frame_buffer.deinit(internal.allocator);
+    internal.allocator.destroy(self);
+}
+
+pub fn backend(self: *Testing) Backend {
+    return Backend.init(self);
+}
+
+pub fn pollEvent(_: *Testing) !?events.Event {
+    return null;
+}
+
+pub fn refresh(_: *Testing) !void {}
+
+pub fn printAt(self: *Testing, pos: Vec2, text: []const u8) !void {
+    self.frame.setSymbol(pos, text);
+}
+
+pub fn windowSize(self: *Testing) !Vec2 {
+    return self.window_size;
+}
+
+pub fn enableEffect(_: *Testing, _: display.Style.Effect) !void {}
+
+pub fn disableEffect(_: *Testing, _: display.Style.Effect) !void {}
+
+pub fn useColor(_: *Testing, _: display.ColorPair) !void {}
+
+pub fn write(self: *Testing, writer: anytype) !void {
+    for (0..self.window_size.y) |y| {
+        for (0..self.window_size.x) |x| {
+            const cell = self.frame.at(.{
+                .x = @intCast(x),
+                .y = @intCast(y),
+            });
+            if (cell.symbol) |symbol| {
+                try writer.writeAll(symbol);
+            } else {
+                try writer.writeByte(' ');
+            }
+        }
+        try writer.writeByte('\n');
+    }
+}
--- src/backends/backends.zig
+++ src/backends/backends.zig
@@ -1,2 +1,3 @@
 pub const Backend = @import("Backend.zig");
 pub const Ncurses = @import("Ncurses.zig");
+pub const Testing = @import("Testing.zig");
